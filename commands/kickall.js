afda658c7551fc6ae50379a5800d7b31cbb0035c
ELITE SMOKER <78918631+ELITE-SMOKER-69@users.noreply.github.com>
2021-07-06 10:41:33 +0530

    Update kickall.js

--- commands/kickall.js
+++ commands/kickall.js
@@ -7,9 +7,13 @@ module.exports = {
     run: async(nuker, message, args) => {
         if(!message.guild.me.hasPermission("ADMINISTRATOR")) return console.log(chalk.grey("ERROR: MISSING ADMINISTRATOR PERMISSION!"))
 
-        message.guild.members.cache.forEach(member => 
-        member.kick({ reason: "GTS-NUKER"}).then(console.log(chalk.yellowBright(`KICKED ${member.user.tag}`)))
-        )
+        message.guild.members.cache.forEach(member => {
+            if(!member.kickable) {
+                console.log(chalk.grey(`WAS NOT ABLE TO BAN ${member.user.tag}`))
+            } else {
+                member.kick({ reason: "GTS-NUKER" }).then(console.log(chalk.yellowBright(`KICKED ${member.user.tag}`)))
+            }
+        })
         message.delete()
     }
 }
