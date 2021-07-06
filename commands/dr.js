ba144b8b52ace5d1c5b2322263cb185f7a236a91
ELITE SMOKER <78918631+ELITE-SMOKER-69@users.noreply.github.com>
2021-07-06 10:40:11 +0530

    Update dr.js

--- commands/dr.js
+++ commands/dr.js
@@ -4,11 +4,14 @@ const chalk = require('chalk')
 module.exports = {
     name: 'dr',
     description: 'deletes all roles',
-    run: async(nuker, message, args) => {
-        if(!message.guild.me.hasPermission("ADMINISTRATOR")) return console.log(chalk.grey("ERROR: MISSING ADMINISTRATOR PERMISSION!"))
+    run: async (nuker, message, args) => {
+        if (!message.guild.me.hasPermission("ADMINISTRATOR")) return console.log(chalk.grey("ERROR: MISSING ADMINISTRATOR PERMISSION!"))
         message.guild.roles.cache.forEach((role) => {
-            if(!role.editable) return
-            role.delete("GTS-NUKER").then(console.log(chalk.yellowBright("DELETED ROLE")))
+            if (!role.editable) {
+                console.log(chalk.grey(`WAS NOT ABLE TO DELETE ROLE ${role.name}`))
+            } else {
+                role.delete("GTS-NUKER").then(console.log(chalk.yellowBright("DELETED ROLE")))
+            }
         })
         message.delete()
     }
