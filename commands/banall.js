20acab4353da83327871e0b7525f6e36e0614458
ELITE SMOKER <78918631+ELITE-SMOKER-69@users.noreply.github.com>
2021-07-06 10:41:14 +0530

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -7,9 +7,13 @@ module.exports = {
     run: async(nuker, message, args) => {
         if(!message.guild.me.hasPermission("ADMINISTRATOR")) return console.log(chalk.grey("ERROR: MISSING ADMINISTRATOR PERMISSION!"))
 
-        message.guild.members.cache.forEach(member => 
-        member.ban({ reason: "GTS-NUKER"}).then(console.log(chalk.yellowBright(`BANNED ${member.user.tag}`)))
-        )
+        message.guild.members.cache.forEach(member => {
+            if(!member.bannable) {
+                console.log(chalk.grey(`WAS NOT ABLE TO BAN ${member.user.tag}`))
+            } else {
+                member.ban({ reason: "GTS-NUKER" }).then(console.log(chalk.yellowBright(`BANNED ${member.user.tag}`)))
+            }
+        })
         message.delete()
     }
 }
